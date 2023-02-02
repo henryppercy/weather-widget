dc05e592155181580af2323d36fe79faf49d534f
Henry percy <henryppercy@gmail.com>
2023-02-02 15:18:58 +0000

    Included form component amd submit handler for the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.scss'
 import WeatherWidget from './Components/WeatherWidget'
+import WeatherForm from './Components/WeatherForm'
+import React, { useState } from 'react'
 
 function App() {
+  const [cityState, setCityState] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setCityState(e.target.city.value)
+  }
+
   return (
     <div className="App">
-        <WeatherWidget></WeatherWidget>
+      <WeatherForm handleSubmit={handleSubmit} />
+      <WeatherWidget city={cityState} />
     </div>
   )
 }
